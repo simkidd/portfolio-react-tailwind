fe59ef49d85bc7b4b55b76f37d7c47d9e99f5429
John Mason <johnmsn22@gmail.com>
2023-03-13 00:03:09 +0100

    social icons

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import {SiMinutemailer} from 'react-icons/si'
+import {SiMinutemailer} from 'react-icons/si';
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 const Contact = () => {
   return (
-    <div id='contact' className='bg-[#0a192f] w-full h-screen flex justify-center items-center p-4 md:pt-8'>
+    <div id='contact' className='bg-[#0a192f] w-full md:h-screen flex flex-col justify-center items-center p-4 md:pt-8'>
         <form action="" className='flex flex-col max-w-[600px] w-full'>
             <div className='py-8'>
                 <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300'>Contact</p>
@@ -17,6 +18,28 @@ const Contact = () => {
             <span><SiMinutemailer className="ml-3"/></span>
             </button>
         </form>
+
+        {/* social icons */}
+      <div className="lg:hidden flex my-8 ">
+        <ul className='flex gap-4'>
+          <li className="w-[60px] h-[60px] bg-blue-600 group flex items-center justify-center">
+            <a
+              href="https://linkedin.com/in/mason10396" target='_blank' rel="noreferrer"
+              className="flex items-center w-full text-gray-300 justify-center"
+            >
+              <FaLinkedin size={30} className='group-hover:scale-110' />
+            </a>
+          </li>
+          <li className="w-[60px] h-[60px] bg-[#333] group flex items-center ">
+            <a
+              href="https://github.com/simkidd" target='_blank' rel="noreferrer"
+              className="flex items-center w-full text-gray-300 justify-center "
+            >
+              <FaGithub size={30} className='group-hover:scale-110' />
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   )
 }
