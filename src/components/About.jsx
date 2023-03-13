2b2fb64182005b6c1d40313db51a8ec81e7da8ab
John Mason <johnmsn22@gmail.com>
2023-03-13 01:26:47 +0100

    social icons

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -16,38 +16,49 @@ const About = () => {
         </div>
         <div className="max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4">
           <div>
-            <p 
-            className="sm:text-right text-4xl font-bold">Hi, I'm John, nice to meet you, Please take a look around.</p>
+            <p className="sm:text-right text-4xl font-bold">
+              Hi, I'm John, nice to meet you, Please take a look around.
+            </p>
           </div>
           <div>
-            <p
-            >I am passionate about building excellent software that improves the lives of those around me. I specialize in creating software for individuals and small-business. What would you do if you had a software expert available at your fingertips?</p>
+            <p>
+              I am passionate about building excellent software that improves
+              the lives of those around me. I specialize in creating software
+              for individuals and small-business. What would you do if you had a
+              software expert available at your fingertips?
+            </p>
           </div>
         </div>
 
-        {/* social icons */}
-      <div className="lg:hidden flex mt-8 ">
-        <ul className='flex gap-4'>
-          <li className="w-[160px] h-[60px] bg-[#565f69] group flex items-center justify-center group">
-            <a
-              href="/" 
-              className="flex items-center justify-between w-full text-gray-300"
-            >
-              Resume
-              <BsFillPersonLinesFill size={30} className='group-hover:scale-110' />
-            </a>
-          </li>
-          <li className="w-[160px] h-[60px] bg-[#6fc2b0] group flex items-center group">
-            <a
-              href="mailto:johnmsn22@gmail.com" 
-              className="flex items-center justify-between w-full text-gray-300"
-            >
-              Email
-              <HiOutlineMail size={30} className='group-hover:scale-110' />
-            </a>
-          </li>
-        </ul>
-      </div>
+        <div className="max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4">
+          <div></div>
+          {/* social icons */}
+          <div className="lg:hidden flex mt-8 ">
+            <ul className="flex gap-4">
+              <li className="w-[160px] h-[60px] bg-[#565f69] group flex items-center justify-center group">
+                <a
+                  href="/"
+                  className="flex items-center justify-between w-full text-gray-300"
+                >
+                  Resume
+                  <BsFillPersonLinesFill
+                    size={30}
+                    className="group-hover:scale-110"
+                  />
+                </a>
+              </li>
+              <li className="w-[160px] h-[60px] bg-[#6fc2b0] group flex items-center group">
+                <a
+                  href="mailto:johnmsn22@gmail.com"
+                  className="flex items-center justify-between w-full text-gray-300"
+                >
+                  Email
+                  <HiOutlineMail size={30} className="group-hover:scale-110" />
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
   );
