2cdaa324fa2b0f70809a6b0482a2e29c06a61a52
John Mason <johnmsn22@gmail.com>
2023-03-27 19:40:34 +0100

    typewriter effect

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,16 @@
 import React from "react";
 import { HiArrowNarrowRight } from "react-icons/hi";
 import { Link } from "react-scroll";
+import { Cursor, useTypewriter } from "react-simple-typewriter";
 
 const Home = () => {
+  const [text] = useTypewriter({
+    words: ['Frontend Developer.'],
+    loop: 0,
+    typeSpeed: 120,
+    deleteSpeed: 80
+  })
+
   return (
     <div id="home" className="bg-[#0a192f] h-screen w-full">
       {/* container */}
@@ -14,7 +22,9 @@ const Home = () => {
         </h1>
 
         <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]">
-          I'm a Frontend Developer.
+          I'm a {" "}
+          <span>{text}</span>
+          <Cursor cursorStyle='<' cursorColor="pink" />
         </h2>
         <p className="text-[#8892b0] py-4 max-w-[700px]">
           I'm a frontend developer specialiizng in building (and occasionally
@@ -22,7 +32,7 @@ const Home = () => {
           building responsive frontend of web applications.
         </p>
         <div className="w-fit">
-          <Link to="work" smooth={true} duration={500} offset={-60} >
+          <Link to="work" smooth={true} duration={500} offset={-60}>
             <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600 ">
               View Work
               <span className="group-hover:rotate-90 duration-300">
